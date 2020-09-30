e29452839d67a1f5ae3bb0b04121e9f32bf3cc39
eduardofrederico <eduardo.frederico@icloud.com>
2020-09-30 16:50:00 -0300

    implementada a funcionalidade para que o .modal seja aberto e/ou fechado.

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,9 @@ const modal = document.querySelector("#modal")
 const close = document.querySelector("#modal .header a")
 
 buttonSearch.addEventListener("click", () => {
-    modal.classList.remove("hide")
+    modal.classList.remove("hide") /*remove a classe*/
 })
 
 close.addEventListener("click", () =>{
-    modal.classList.add("hide")
+    modal.classList.add("hide") /*adiciona a classe*/
 })
